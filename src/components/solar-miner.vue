eb9a0f1ab150cb1c7937512999b47d399f6aea55
Stuart Jones <stuart@savvywombat.com.au>
2021-03-26 10:49:25 +1030

    componentized planet

--- src/components/solar-miner.vue
+++ src/components/solar-miner.vue
@@ -22,49 +22,24 @@
             </div>
         </div>
 
-        <div class="border grid grid-cols-card-2 p-1 my-2" v-for="(planet, index) in planets" :key="index">
-            <h1 class="col-start-left col-end-center font-bold">{{ planet.name }}</h1>
-            <div class="col-start-center col-end-right text-right">{{ planet.buildings }} / {{ planet.buildLimit }}</div>
+        <planet v-for="(planet, index) in planets"
+                :key="index"
+                class="border grid grid-cols-card-2 p-1 my-2"
+                :planet="planet"
+                @buy-miner="buyMiner(planet)"
+                @buy-storage="buyStorage(planet)"
+                @buy-ship="buyShip(planet)"
+                @upgrade-ships="upgradeShips(planet)"
+                @abandon="abandon(planet)"
+        >
 
-            <div class="col-start-left col-end-center">Unmined resources</div>
-            <div class="col-start-center col-end-right text-right">{{ planet.unminedResources }}</div>
-
-            <div class="col-start-left col-end-center">Mining capacity</div>
-            <div class="col-start-center col-end-right text-right">{{ planet.miningCapacity }}</div>
-
-            <div class="col-start-left col-end-center">Storage</div>
-            <div class="col-start-center col-end-right text-right">{{ planet.storedResources }} / {{ planet.storageCapacity }}</div>
-
-            <div class="col-start-left col-end-center">Ships</div>
-            <div class="col-start-center col-end-right text-right">
-                {{ planet.ships.filter((ship) => { return ship.status === 'loading'}).length }} /
-                {{ planet.ships.filter((ship) => { return ship.status === 'toEarth'}).length }} /
-                {{ planet.ships.filter((ship) => { return ship.status === 'unloading'}).length }} /
-                {{ planet.ships.filter((ship) => { return ship.status === 'toPlanet'}).length }} /
-                {{ planet.ships.length }}
-            </div>
-
-            <div class="col-start-left col-end-right">
-                <button class="border hover:bg-green-500 m-1 p-1 "
-                        @click="buyMiner(planet)">Buy miner (100)</button>
-
-                <button class="border hover:bg-green-500 m-1 p-1 "
-                        @click="buyStorage(planet)">Buy storage (100)</button>
-
-                <button class="border hover:bg-green-500 m-1 p-1 "
-                        @click="buyShip(planet)">Buy ship (100)</button>
-
-                <button class="border hover:bg-green-500 m-1 p-1 "
-                        @click="upgradeShips(planet)">Upgrade ships (100)</button>
-
-                <button class="border hover:bg-red-500 m-1 p-1 "
-                        @click="abandon(planet)">Abandon</button>
-            </div>
-        </div>
+        </planet>
     </div>
 </template>
 
 <script>
+import Planet from './planet';
+
 // TODO
 // 1. cost to unlock planets beyond moon
 // 2. increase price of new buildings
@@ -80,6 +55,10 @@
 // 12. Charts
 // 13. Ship speed upgrades apply to new ships, too?
 export default {
+    components: {
+        planet: Planet,
+    },
+
     data() {
       return {
         timer: null,
